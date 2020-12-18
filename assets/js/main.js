edaf5b8ae7fb2bcaa6952c9cf1316e7d7eb2da7e
Max Ivanov <waxivanov@gmail.com>
2020-12-18 11:56:43 +0200

    refactor

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// SWIPER
+
+new Swiper('.swiper-container', {
+  // grabCursor: true,
+  autoHeight: true,
+  slidesPerView: 'auto',
+  spaceBetween: 15,
+  loop: true,
+  // loopedSlides: 16,
+  freeMode: true,
+});
